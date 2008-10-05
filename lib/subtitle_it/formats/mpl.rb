503f4e57fea26513952b8685500a359d0f4463e7
Marcos Augusto <x@nofxx.com>
2008-10-05 06:53:15 -0300

    rspec runs again

--- lib/subtitle_it/formats/mpl.rb
+++ lib/subtitle_it/formats/mpl.rb
@@ -9,10 +9,10 @@ module Formats
   include PlatformEndLine
   def parse_mpl
     @raw.to_a.inject([]) do |i,l|
-		      line_data = l.scan(/^\[([0-9]{1,})\]\[([0-9]{1,})\](.+)$/)
-		      line_data = line_data.at 0
-		      time_on, time_off, text = line_data
-		      time_on, time_off = [time_on.to_i, time_off.to_i].map { |t| t.to_i * 1000 }	    
+      line_data = l.scan(/^\[([0-9]{1,})\]\[([0-9]{1,})\](.+)$/)
+      line_data = line_data.at 0
+      time_on, time_off, text = line_data
+      time_on, time_off = [time_on.to_i, time_off.to_i].map { |t| t.to_i * 1000 }	    
       i << Subline.new(time_on, time_off, text.chomp)
     end
   end
